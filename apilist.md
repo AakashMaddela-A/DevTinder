58f39718f0e65faea27220fdbd62801a7ecbfd4e
Aakash Maddela <maddelaaakash@gmail.com>
2026-03-20 13:30:16 +0530

    review api

--- apilist.md
+++ apilist.md
@@ -1,22 +1,23 @@
 # Devtinder apis
 
-auth router
+## auth router
 -POST /signup
 -POST /login
 -POST /logout
 
-profile router
+## profile router
 -GET /profile/view
 -PATCH /profile/edit
 -PATCH /profile/password
 
-connection request router
+## connection request router
 -POST /request/send/intrested/:userid
 -POST /request/send/ignore/:userid
+
 -POST /request/review/accepted/:requestID
 -POST /request/review/rejected/:requestID
 
-user router
+## user router
 -GET /connections
 -GET /requests/received
 -GET /feed/
--- src/routes/request.js
+++ src/routes/request.js
@@ -62,4 +62,47 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
     }
 })
 
+requestRouter.post("/request/review/:status/:requestId",userAuth,async(req,res)=>{
+    
+    try{
+        const loggedInUser=req.user;
+        const {status,requestId}= req.params;
+
+        const allowedStatus=["accepted","rejected"];
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message: "Status not allowed!!"})
+        }
+
+        // in databse there or not
+        const connectionRequest= await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested",
+        });
+
+
+        console.log("DB Data:", connectionRequest);
+        console.log("Logged User:", loggedInUser._id);
+
+
+
+        if(!connectionRequest){
+            return res.status(404).json({message: "Connection request not found"})
+        }
+
+        connectionRequest.status=status;
+
+        const data = await connectionRequest.save();
+
+        res.json({message: "Connection requested " + status, data });
+
+
+    }
+    catch(err){
+        res.status(400).send("ERROR: " + err.message);
+    }
+   
+
+})
+
 module.exports = requestRouter;
